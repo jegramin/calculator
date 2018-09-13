4bbfb780e6b119fa40e7458139e5f52c9bef1833
jegramin <jegroamin@gmail.com>
2018-09-13 16:02:50 +0300

    initial commit

---
+++ calc.js
@@ -0,0 +1,336 @@
+
+	document.getElementById("zero").onclick= function(){
+	var a = document.getElementById("zero").innerHTML;
+	document.getElementById("firstInput").value += a;
+}
+	
+	document.getElementById("1").onclick= function(){
+	var a = document.getElementById("1").innerHTML;
+	document.getElementById("firstInput").value += a;
+}
+	
+	document.getElementById("2").onclick= function(){
+	var a = document.getElementById("2").innerHTML;
+	document.getElementById("firstInput").value += a;
+}
+	
+	document.getElementById("3").onclick= function(){
+	var a = document.getElementById("3").innerHTML;
+	document.getElementById("firstInput").value += a;
+}
+	
+	document.getElementById("4").onclick= function(){
+	var a = document.getElementById("4").innerHTML;
+	document.getElementById("firstInput").value += a;
+}
+	
+	document.getElementById("5").onclick= function(){
+	var a = document.getElementById("5").innerHTML;
+	document.getElementById("firstInput").value += a;
+}
+	
+	document.getElementById("6").onclick= function(){
+	var a = document.getElementById("6").innerHTML;
+	document.getElementById("firstInput").value += a;
+}
+	
+	document.getElementById("7").onclick= function(){
+	var a = document.getElementById("7").innerHTML;
+	document.getElementById("firstInput").value += a;
+}
+
+	document.getElementById("8").onclick= function(){
+	var a = document.getElementById("8").innerHTML;
+	document.getElementById("firstInput").value += a;
+}
+
+	document.getElementById("9").onclick= function(){
+	var a = document.getElementById("9").innerHTML;
+	document.getElementById("firstInput").value += a;
+}
+
+
+
+	document.getElementById("division").onclick = function(){
+		var b  = document.getElementById("firstInput").value ;
+		if(true){
+
+
+				document.getElementById("zero").onclick= function(){
+	var a = document.getElementById("zero").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("1").onclick= function(){
+	var a = document.getElementById("1").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("2").onclick= function(){
+	var a = document.getElementById("2").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("3").onclick= function(){
+	var a = document.getElementById("3").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("4").onclick= function(){
+	var a = document.getElementById("4").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("5").onclick= function(){
+	var a = document.getElementById("5").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("6").onclick= function(){
+	var a = document.getElementById("6").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("7").onclick= function(){
+	var a = document.getElementById("7").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+
+	document.getElementById("8").onclick= function(){
+	var a = document.getElementById("8").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+
+	document.getElementById("9").onclick= function(){
+	var a = document.getElementById("9").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+		}
+		
+		
+		document.getElementById("equal").onclick=function(){
+			document.getElementById("output1").value= b/document.getElementById("secondInput").value;
+		}
+
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		document.getElementById("mult").onclick = function(){
+		var b  = document.getElementById("firstInput").value ;
+		if(true){
+
+
+				document.getElementById("zero").onclick= function(){
+	var a = document.getElementById("zero").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("1").onclick= function(){
+	var a = document.getElementById("1").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("2").onclick= function(){
+	var a = document.getElementById("2").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("3").onclick= function(){
+	var a = document.getElementById("3").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("4").onclick= function(){
+	var a = document.getElementById("4").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("5").onclick= function(){
+	var a = document.getElementById("5").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("6").onclick= function(){
+	var a = document.getElementById("6").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("7").onclick= function(){
+	var a = document.getElementById("7").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+
+	document.getElementById("8").onclick= function(){
+	var a = document.getElementById("8").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+
+	document.getElementById("9").onclick= function(){
+	var a = document.getElementById("9").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+		}
+		
+		
+
+		document.getElementById("equal").onclick=function(){
+			document.getElementById("output1").value= b*document.getElementById("secondInput").value;
+		}
+		}
+
+
+
+
+
+
+
+
+
+		document.getElementById("sub").onclick = function(){
+		var b  = document.getElementById("firstInput").value ;
+		if(true){
+
+
+				document.getElementById("zero").onclick= function(){
+	var a = document.getElementById("zero").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("1").onclick= function(){
+	var a = document.getElementById("1").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("2").onclick= function(){
+	var a = document.getElementById("2").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("3").onclick= function(){
+	var a = document.getElementById("3").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("4").onclick= function(){
+	var a = document.getElementById("4").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("5").onclick= function(){
+	var a = document.getElementById("5").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("6").onclick= function(){
+	var a = document.getElementById("6").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("7").onclick= function(){
+	var a = document.getElementById("7").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+
+	document.getElementById("8").onclick= function(){
+	var a = document.getElementById("8").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+
+	document.getElementById("9").onclick= function(){
+	var a = document.getElementById("9").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+		}
+		document.getElementById("equal").onclick=function(){
+		document.getElementById("output1").value= b-document.getElementById("secondInput").value;
+	}
+
+		}
+
+
+
+
+
+
+
+
+
+		document.getElementById("sum").onclick = function(){
+		var b  = document.getElementById("firstInput").value ;
+		if(true){
+
+
+				document.getElementById("zero").onclick= function(){
+	var a = document.getElementById("zero").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("1").onclick= function(){
+	var a = document.getElementById("1").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("2").onclick= function(){
+	var a = document.getElementById("2").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("3").onclick= function(){
+	var a = document.getElementById("3").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("4").onclick= function(){
+	var a = document.getElementById("4").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("5").onclick= function(){
+	var a = document.getElementById("5").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("6").onclick= function(){
+	var a = document.getElementById("6").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+	
+	document.getElementById("7").onclick= function(){
+	var a = document.getElementById("7").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+
+	document.getElementById("8").onclick= function(){
+	var a = document.getElementById("8").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+
+	document.getElementById("9").onclick= function(){
+	var a = document.getElementById("9").innerHTML;
+	document.getElementById("secondInput").value += a;
+}
+		}
+		document.getElementById("equal").onclick=function(){
+		document.getElementById("output1").value=parseInt(b)+parseInt(document.getElementById("secondInput").value);
+}
+
+		}
+
+
+		document.getElementById("c").onclick=function(){
+			document.getElementById("secondInput").value=null;
+			document.getElementById("output1").value=null;
+			document.getElementById("firstInput").value=null;
+		}
